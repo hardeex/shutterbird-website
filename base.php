1fb6a26ed26b758f00786dd0f00fd7672ee83241
Adewale <jamiuadewaleyusuf@gmail.com>
2023-10-21 11:30:18 +0100

    creating the layout page

---
+++ base.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="">
+    <meta name="author" content="DynamicLayers">
+    <?php $title = 'Base' ?>
+    <title><?php echo $title; ?> </title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+
+    <!-- Font Awesome Icons CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Themify Icons CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- Elegant Font Icons CSS -->
+    <link rel="stylesheet" href="css/elegant-font-icons.css">
+    <!-- Elegant Line Icons CSS -->
+    <link rel="stylesheet" href="css/elegant-line-icons.css">
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Venobox CSS -->
+    <link rel="stylesheet" href="css/venobox/venobox.css">
+	<!-- OWL-Carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <!-- Slick Nav CSS -->
+    <link rel="stylesheet" href="css/slicknav.min.css">
+    <!-- Css Animation CSS -->
+    <link rel="stylesheet" href="css/css-animation.min.css">
+    <!-- Nivo Slider CSS -->
+    <link rel="stylesheet" href="css/nivo-slider.css">
+	<!-- Main CSS -->
+    <link rel="stylesheet" href="css/main.css">
+	<!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+
+
+<body>
+
+    <div class="site-preloader-wrap">
+            <div class="spinner"></div>
+    </div>
+
+        <header id="header" class="header-section">
+            <div class="top-header">
+                <div class="container">
+                    <div class="top-content-wrap row">
+                        <div class="col-sm-8">
+                            <ul class="left-info">
+                                <li><a href="#"><i class="ti-email"></i>shutterbirdng@gmail.com</a></li>
+                                <li><a href="#"><i class="ti-mobile"></i>+(234) 8136 552 100</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 d-none d-md-block">
+                            <ul class="right-info">
+                                <li><a href="https://www.Facebook.com/Shutterbird.ng"><i class="fa fa-facebook"></i></a></li>
+				                <li><a href="https://web.whatsapp.com/send?phone=2347060969306&app=facebook&entry_point=page_cta&fbclid=IwAR1mzyS4XBDXTsVkJsOqz4ebhuDajXNfto69MyL6OcGP7giYB98REAbcpRc"><i class="fa fa-whatsapp"></i></a></li>
+                                <li><a href="https://twitter.com/urennaamadi"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="https://www.instagram.com/Shutterbird.ng"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="https://t.me/joinchat/edZp9EGF5sowMmI0"><i class="fa fa-telegram"></i></a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom-header">
+                <div class="container">
+                    <div class="bottom-content-wrap row">
+                        <div class="col-sm-4">
+                            <div class="site-branding">
+                                <a href="#"><img src="img/logo.png" alt="Brand"></a>
+                            </div>
+                        </div>
+                      <div class="col-sm-8 text-right">
+                                   <ul id="mainmenu" class="nav navbar-nav nav-menu">
+                                <li class="active"> <a href="index.html">Home</a></li>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="more.html">Event</a></li>
+                                <li><a href="motivate.html">Motivate</a></li>
+                               
+                            </ul>
+                           
+                       </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </header>
+
+ 
+
+
+        <footer style="margin-top: 5rem;">
+        <section class="widget-section padding">
+            <div class="container">
+                <div class="widget-wrap row">
+                    <div class="col-md-4 xs-padding" style="margin-top: 5rem;">
+                        <div class="widget-content">
+                            <img src="img/logo-light.png" alt="logo">
+                            <p> The character Shutterbird is the first African superhero character coming as a movie in cinemas. 
+				                She is a superhero with her superpowers in her golden camera which possesses super abilities; super strength, speed, super reflexes, durability, healing factor,
+				                night vision, aperture and heightened senses alert, acrobatics, stamina and agility, camera freezing and shooting capabilities.</p>
+                            <ul class="social-icon">
+				<li><a href="https://www.Facebook.com/Shutterbird.ng"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="https://web.whatsapp.com/send?phone=2347060969306&app=facebook&entry_point=page_cta&fbclid=IwAR1mzyS4XBDXTsVkJsOqz4ebhuDajXNfto69MyL6OcGP7giYB98REAbcpRc"><i class="fa fa-whatsapp"></i></a></li>
+                                <li><a href="https://twitter.com/urennaamadi"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="https://www.instagram.com/Shutterbird.ng"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="https://t.me/joinchat/edZp9EGF5sowMmI0"><i class="fa fa-telegram"></i></a></li>
+                                
+                                  </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-4 xs-padding" style="margin-top: 5rem;">
+                        <div class="widget-content">
+                            <h3>Recent Updates</h3>
+                             <ul class="widget-link">
+                                <li><a href="news1.php">Shutterbird character launch </a></li>
+                                <li><a href="news2.php">Urena Amadi character art exhibition </a></li>
+                                <li><a href="news3.php">Shutterbird 3-D model </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-4 xs-padding" style="margin-top: 5rem;">
+                        <div class="widget-content">
+                            <h3>Our Location</h3>
+                            <ul class="address">
+                                <li><i class="ti-email"></i> shutterbirdng@gmail.com </li>
+                                <li><i class="ti-mobile"></i> +(234) 8136 552 100 </li>
+                                <li><i class="ti-world"></i> www.afriwood.org</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><!-- ./Widget Section -->
+        
+        <footer class="footer-section">
+			<div class="container">
+                <div class="row">
+                    <div class="col-md-6 sm-padding">
+                    <div class="copyright">&copy; <?php echo date("Y"); ?> Shutterbird Website Powered by Afriwood</div>
+                   </div>
+                   
+                </div>
+			</div>
+		</footer><!-- /Footer Section -->
+        
+		<a data-scroll href="#header" id="scroll-to-top"><i class="arrow_up"></i></a>
+	
+		<!-- jQuery Lib -->
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="js/vendor/bootstrap.min.js"></script>
+		<!-- Tether JS -->
+		<script src="js/vendor/tether.min.js"></script>
+        <!-- Imagesloaded JS -->
+        <script src="js/vendor/imagesloaded.pkgd.min.js"></script>
+		<!-- OWL-Carousel JS -->
+		<script src="js/vendor/owl.carousel.min.js"></script>
+		<!-- isotope JS -->
+		<script src="js/vendor/jquery.isotope.v3.0.2.js"></script>
+		<!-- Smooth Scroll JS -->
+		<script src="js/vendor/smooth-scroll.min.js"></script>
+		<!-- venobox JS -->
+		<script src="js/vendor/venobox.min.js"></script>
+        <!-- ajaxchimp JS -->
+        <script src="js/vendor/jquery.ajaxchimp.min.js"></script>
+        <!-- Counterup JS -->
+		<script src="js/vendor/jquery.counterup.min.js"></script>
+        <!-- waypoints js -->
+		<script src="js/vendor/jquery.waypoints.v2.0.3.min.js"></script>
+        <!-- Slick Nav JS -->
+        <script src="js/vendor/jquery.slicknav.min.js"></script>
+        <!-- Nivo Slider JS -->
+        <script src="js/vendor/jquery.nivo.slider.pack.js"></script>
+        <!-- Letter Animation JS -->
+		<script src="js/vendor/letteranimation.min.js"></script>
+        <!-- Wow JS -->
+		<script src="js/vendor/wow.min.js"></script>
+		<!-- Contact JS -->
+		<script src="js/contact.js"></script>
+		<!-- Main JS -->
+		<script src="js/main.js"></script>
+        </footer>
+		
+</body>
+</html>
